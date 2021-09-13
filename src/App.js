686cf488af93b4d97fa8dc4cdd51971dad91c916
yellowgh0st <augustin.be@protonmail.com>
2021-09-13 02:05:10 +0200

    Create tab routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch, Redirect, Link } from 'react-router-dom'
 import { ChakraProvider, Heading, Flex, Box, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react'
 import vether from './themes/vether'
 import { Header } from './components/Header'
@@ -20,97 +20,123 @@ const App = () => {
 					<Header width='100%'
 						p='1.2rem 1rem'
 						justifyContent='center'/>
-					<Tabs
-						colorScheme='white'
-						isLazy>
-						<TabList
-							p='0 1rem 0.4rem'
-						>
-							<Tab
-								minW='233px'
-								flexDir='column'
-							>
-								<div style={{
-									textTransform: 'uppercase',
-									fontSize: '0.78rem',
-									fontWeight: 'bold',
-									opacity: '0.6',
-								}}>Total</div>
-								<div>Value Burnt</div>
-							</Tab>
-							<Tab
-								minW='235px'
-								flexDir='column'
-							>
-								<div style={{
-									textTransform: 'uppercase',
-									fontWeight: 'bold',
-									fontSize: '0.78rem',
-									opacity: '0.6',
-								}}>Top</div>
-								<div>Holder Accounts</div>
-							</Tab>
-						</TabList>
-						<Box
-							h='auto'
-							bg='rgb(42, 42, 42)'
-							borderRadius='1.4rem'
-							border='1px solid rgba(255, 255, 255, 0.2)'
-							m='1rem'
-							p='3.4rem 2.3rem 3.3rem'>
-							<GlobalsPanel
-								pt='0.3rem'
-								minH='121.6px'/>
-							<TabPanels>
-								<TabPanel>
-									<BurnChart
-										height='calc(100vh - 465.1167px)'
-										pb='160px'
-									/>
-								</TabPanel>
-								<TabPanel
-									display='flex'
-									flexDir='row'
-									justifyContent='center'
-								>
-									<Flex
-										width='49%'
-										flexDir='column'>
-										<Heading
-											as='h4'
-											size='xs'
-											fontWeight='normal'
-											fontStyle='italic'
-											lineHeight='1'
-											marginInlineStart='1.1rem'
-											mb='5px'
-											opacity='0.8'
-											textStyle='noLigs'>
-												Top Holder Accounts
-										</Heading>
-										<HolderChart
-											flexDirection='column'
-											justifyContent='center'
-											alignItems='center'
-											height='calc(100vh - 484.1167px)'
-										/>
-									</Flex>
-									<Flex
-										width='51%'
-										alignItems='center'>
-										<HolderList
-											width='100%'
-											justifyContent='center'
-										/>
-									</Flex>
-								</TabPanel>
-							</TabPanels>
-						</Box>
-					</Tabs>
+					<Switch>
+						<Route path='/' exact render={() =>
+							<Redirect to={'/burns'} />
+						} />
+						<Route path='/burns' exact render={() =>
+							<AnalyticsTabs tabIndex={0}/>
+						}/>
+						<Route path='/holders' exact render={() =>
+							<AnalyticsTabs tabIndex={1}/>
+						}/>
+						<Route path='*' render={() =>
+							<Redirect to={'/burns'} />
+						} />
+					</Switch>
 					<Footer/>
 				</Flex>
 			</ChakraProvider>
 		</Router>
+	)
+}
+
+const AnalyticsTabs = (props) => {
+
+	return(
+		<Tabs
+			// eslint-disable-next-line react/prop-types
+			index={Number(props.tabIndex)}
+			colorScheme='white'
+			isLazy>
+			<TabList
+				p='0 1rem 0.4rem'
+			>
+				<Link to='/burns'>
+					<Tab
+						minW='233px'
+						flexDir='column'
+					>
+						<div style={{
+							textTransform: 'uppercase',
+							fontSize: '0.78rem',
+							fontWeight: 'bold',
+							opacity: '0.6',
+						}}>Total</div>
+						<div>Value Burnt</div>
+					</Tab>
+				</Link>
+				<Link to='/holders'>
+					<Tab
+						minW='235px'
+						flexDir='column'
+					>
+						<div style={{
+							textTransform: 'uppercase',
+							fontWeight: 'bold',
+							fontSize: '0.78rem',
+							opacity: '0.6',
+						}}>Top</div>
+						<div>Holder Accounts</div>
+					</Tab>
+				</Link>
+			</TabList>
+			<Box
+				h='auto'
+				bg='rgb(42, 42, 42)'
+				borderRadius='1.4rem'
+				border='1px solid rgba(255, 255, 255, 0.2)'
+				m='1rem'
+				p='3.4rem 2.3rem 3.3rem'>
+				<GlobalsPanel
+					pt='0.3rem'
+					minH='121.6px'/>
+				<TabPanels>
+					<TabPanel>
+						<BurnChart
+							height='calc(100vh - 465.1167px)'
+							pb='160px'
+						/>
+					</TabPanel>
+					<TabPanel
+						display='flex'
+						flexDir='row'
+						justifyContent='center'
+					>
+						<Flex
+							width='49%'
+							flexDir='column'>
+							<Heading
+								as='h4'
+								size='xs'
+								fontWeight='normal'
+								fontStyle='italic'
+								lineHeight='1'
+								marginInlineStart='1.1rem'
+								mb='5px'
+								opacity='0.8'
+								textStyle='noLigs'>
+							Top Holder Accounts
+							</Heading>
+							<HolderChart
+								flexDirection='column'
+								justifyContent='center'
+								alignItems='center'
+								height='calc(100vh - 484.1167px)'
+							/>
+						</Flex>
+						<Flex
+							width='51%'
+							alignItems='center'>
+							<HolderList
+								width='100%'
+								justifyContent='center'
+							/>
+						</Flex>
+					</TabPanel>
+				</TabPanels>
+			</Box>
+		</Tabs>
 	)
 }
 
